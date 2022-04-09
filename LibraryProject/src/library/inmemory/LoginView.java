6eaa7054cb47f9a19078ae49d266aaaab4753cdf
SEOKYUNG <lastsun4@naver.com>
2022-04-10 08:39:24 +0900

    수정?

--- LibraryProject/src/library/inmemory/LoginView.java
+++ LibraryProject/src/library/inmemory/LoginView.java
@@ -13,7 +13,7 @@ public class LoginView {
 		System.out.println("==============================================================");
 		System.out.println("\t \t \t <일반회원 로그인>");
 		System.out.println("==============================================================");
-
+ 
 		lof.loginInput();
 
 	}
